d891b5ac3a743993a455072029f9f912db96a9e4
CyberX-club <cybersecurity@dpsrkp.net>
2024-10-29 05:19:05 +0000

    Bravo Six! Going Dark

--- Endpoints.py
+++ Endpoints.py
@@ -1,5 +1,7 @@
 class Endpoints:
 
     get_magazaine_data = '/magazine/<slug>'
+    get_resources = '/resources'
     get_random_image = '/random-image'
     magazine_base_url = lambda spreadsheetId,key,range : f"https://sheets.googleapis.com/v4/spreadsheets/{spreadsheetId}/values/{range}?key={key}"
+    resources_base_url = lambda spreadsheetId,key,range : f"https://sheets.googleapis.com/v4/spreadsheets/{spreadsheetId}/values/{range}?key={key}"
--- app.py
+++ app.py
@@ -15,49 +15,76 @@ IMAGES_DIR = f'{os.path.dirname(__file__)}/{"images"}'
 
 key = os.environ['SHEETS_API_KEY']
 spreadsheetId = os.environ['SPREADSHEET_ID']
-magazine_base_url = Endpoints.magazine_base_url(spreadsheetId, key, "A1:E")
+resSpreadsheetId = os.environ['RESOURCES_SPREADSHEET_ID']
 
-class Article:
+magazine_base_url = Endpoints.magazine_base_url(spreadsheetId, key, "A1:E")
+resources_base_url = Endpoints.resources_base_url(resSpreadsheetId, key, "A:F")
+
+# base entity simply defines the class so that we have a definite structure 
+class BaseEntity:
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def to_dict(self):
+        return {attr: getattr(self, attr) for attr in self.__dict__}
+
+# these classes extend BaseEntity and define the structure of the data
+
+class Article(BaseEntity):
     def __init__(self, title='', url='', author='', images='', content=''):
-        self.title = title
-        self.url = url
-        self.author = author
-        self.images = images.split(",") if images else []
-        self.content = content
+        super().__init__(title=title, url=url, author=author, images=images.split(",") if images else [], content=content)
 
     def __repr__(self):
         return (f"Article(title={self.title}, url={self.url}, "
                 f"author={self.author}, images={self.images}, content={self.content})")
 
-    def to_dict(self):
-        return {
-            "title": self.title,
-            "url": self.url,
-            "author": self.author,
-            "images": self.images,
-            "content": self.content
-        }
+class Resource(BaseEntity):
+    def __init__(self, label, description="", img="", author="", postedAt="", urls=[]):
+        super().__init__(label=label, description=description, img=img, author=author, postedAt=postedAt,
+                         urls=list(filter(None, urls.split(","))) if urls else [])
 
-def parse_magazine_spreadsheet_data(data):
-    if not data or len(data) < 2:
-        return []
-
-    header_mapping = {
+    def __repr__(self):
+        return (f"Resource(label={self.label}, description={self.description}, "
+                f"img={self.img}, author={self.author}, postedAt={self.postedAt}, urls={self.urls})")
+header_mappings = {
+    'articles': {
         "Title": "title",
         "Url": "url",
         "Author": "author",
         "Images (Seperated by comma)": "images",
         "Content": "content"
+    },
+    'resources': {
+        "Label": "label",
+        "Description": "description",
+        "Img": "img",
+        "Author": "author",
+        "PostedAt": "postedAt",
+        "Urls": "urls"
     }
+}
 
+def parse_spreadsheet_data(data, entity_type):
+    if not data or len(data) < 2:
+        return []
+
+    header_mapping = header_mappings[entity_type]
     headers = data[0]
-    articles = []
+    entities = []
 
     for row in data[1:]:
-        article_data = {header_mapping[header]: (value if value else '') for header, value in zip(headers, row)}
-        articles.append(Article(**article_data))
+        entity_data = {header_mapping[header]: (value if value else '') for header, value in zip(headers, row)}
+        if entity_type == 'articles':
+            entities.append(Article(**entity_data))
+        elif entity_type == 'resources':
+            entities.append(Resource(**entity_data))
 
-    return articles
+    return entities
+
+
+
+
 
 @app.route("/")
 def hello_world():
@@ -65,13 +92,11 @@ def hello_world():
 
 @app.route(Endpoints.get_magazaine_data)
 def magazine_data(slug):
-
     error = None
 
     try:
         data = Endpoint(magazine_base_url, "GET").fetch()[0]['values']
-
-        articles = parse_magazine_spreadsheet_data(data)
+        articles = parse_spreadsheet_data(data, 'articles')
 
         for article in articles:
             if article.url == slug:
@@ -81,7 +106,7 @@ def magazine_data(slug):
         error = str(e)
 
     return jsonify({
-        "error" : error if error else "Article not found",
+        "error": error if error else "Article not found",
     })
 
 @app.route(Endpoints.get_random_image)
@@ -93,6 +118,26 @@ def random_image():
     random_image = random.choice(images)
     
     return send_file(f"{IMAGES_DIR}/{random_image}")
+
+
+@app.route(Endpoints.get_resources)
+def resources():
+    error = None
+
+    try:
+        data = Endpoint(resources_base_url, "GET").fetch()[0]['values']
+        resources = parse_spreadsheet_data(data, 'resources')
+        return jsonify({
+            "resources": [resource.to_dict() for resource in resources]
+        })
+    except Exception as e:
+        error = str(e)
+
+    return jsonify({
+        "error": error if error else "No resources found",
+        "resources": []
+    })
+
 
 
 if __name__=="__main__":
